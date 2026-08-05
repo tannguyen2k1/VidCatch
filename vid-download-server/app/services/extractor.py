a05b4cbe78ff820f18dc57eb4cdef9118d3cb666
tannguyen2001 <ntan7928@gmail.com>
2026-08-05 20:31:22 +0700

    feat: implement YtDlpExtractor service for metadata extraction and media downloading

--- vid-download-server/app/services/extractor.py
+++ vid-download-server/app/services/extractor.py
@@ -179,10 +179,6 @@ class YtDlpExtractor:
             'color': 'no_color',
             'ratelimit': 3 * 1024 * 1024, # 3MB/s
             'merge_output_format': 'mp4',
-            'postprocessors': [{
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4',
-            }],
         })
         
         def guarded_progress_hook(d):
